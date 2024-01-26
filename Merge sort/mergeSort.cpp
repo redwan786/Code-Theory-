b222fee73c7afa0c1ac5832502bb3cb53380cc1f
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-01-26 10:53:33 +0600

    update

--- Merge sort/mergeSort.cpp
+++ Merge sort/mergeSort.cpp
@@ -11,38 +11,44 @@ int main()
 
     int arrA[sizeA], arrB[sizeB];
 
-    cout << "Enter " << sizeA << "elements of 1st array :";
+    cout << "Enter " << sizeA << " elements of 1st array :";
     for(int i = 0; i < sizeA; i++)
     {
         cin >> arrA[i];
     }
 
-    cout << "Enter " << sizeB << "elements of 2nd array :";
+    cout << "Enter " << sizeB << " elements of 2nd array :";
     for(int i = 0; i < sizeB; i++)
     {
         cin >> arrB[i];
     }
 
+                    //*** merge 2 sorted array and 3rd sorted merge array code*****
+
     int sizeC = sizeA + sizeB;
     int arrC[sizeC];
 
+    arrA[sizeA] = 999999;
+    arrB[sizeB] = 999999;
 
-    for(int i = 0, j = 0, k = 0; (i < sizeA || j < sizeB || k < sizeC); )
+
+    for(int k = 0, i = 0, j = 0; k < sizeC; k++)
     {
         if(arrA[i] < arrB[j])
         {
             arrC[k] = arrA[i];
             i++;
-            k++;
         }
         else
         {
             arrC[k] = arrB[j];
             j++;
-            k++;
+
         }
 
     }
+
+                    // print the merge sorted array
 
     for(int i = 0; i < sizeC; i++)
     {
---
+++ Merge sort/merge_two_sorted_array.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void mergeTwoSortedArray(int arrA[],int arrB[],int sizeA, int sizeB)
+{
+    int size_merge = sizeA + sizeB;
+    int arr_merge[size_merge];
+
+    arrA[sizeA] = 999999;
+    arrB[sizeB] = 999999;
+
+
+    for(int k = 0, i = 0, j = 0; k < size_merge; k++)
+    {
+        if(arrA[i] < arrB[j])
+        {
+            arr_merge[k] = arrA[i];
+            i++;
+        }
+        else
+        {
+            arr_merge[k] = arrB[j];
+            j++;
+
+        }
+
+    }
+
+    // print the merge sorted array
+
+    for(int i = 0; i < size_merge; i++)
+    {
+        cout << arr_merge[i] << " ";
+    }
+
+
+}
+int main()
+{
+    cout << "***You have to input two sorted array***" << endl;
+    int sizeA, sizeB;
+    cout << "Enter 1st array size :";
+    cin >> sizeA;
+    cout << "Enter 2nd array size :";
+    cin >> sizeB;
+
+    int arrA[sizeA], arrB[sizeB];
+
+    cout << "Enter " << sizeA << " elements of 1st array :";
+    for(int i = 0; i < sizeA; i++)
+    {
+        cin >> arrA[i];
+    }
+
+    cout << "Enter " << sizeB << " elements of 2nd array :";
+    for(int i = 0; i < sizeB; i++)
+    {
+        cin >> arrB[i];
+    }
+
+    mergeTwoSortedArray(arrA,arrB,sizeA,sizeB);
+
+
+    return 0;
+}
+
