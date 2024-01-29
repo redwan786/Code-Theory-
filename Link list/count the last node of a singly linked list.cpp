f1e985aad6b36a9b3f5c20dd44d402944ad3b6dc
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-01-29 22:13:41 +0600

    Length of the singly linked list edit korbo akhon

---
+++ Link list/count the last node of a singly linked list.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+node *root = NULL;
+
+void find_length()
+{
+    int length_count =0;
+    node *curr_node=root;
+    while(curr_node->next!=NULL)
+    {
+        curr_node=curr_node->next;
+        length_count++;
+    }
+    cout << "Length of the singly linked list is : "<<length_count+1;
+
+}
+
+
+int main()
+{
+    node *p, *q, *r, *s;
+    p = new node();
+    q = new node();
+    r = new node();
+    s = new node();
+
+    p->data=3;
+    q->data=8;
+    r->data=10;
+    s->data=12;
+
+    p->next=q;
+    q->next=r;
+    r->next=s;
+    s->next=NULL;
+
+    root=p;
+
+    find_length();
+
+
+}
