dda78f7701f25696bbcf097403fee112db543265
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-02-01 00:09:50 +0600

    insert a node in the last.cpp

--- Link list/insert a node in the last.cpp
+++ Link list/insert a node in the last.cpp
@@ -1,36 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 struct node
 {
     int data;
     node *next;
 };
+
 node *root = NULL;
+
 void printing()
 {
     node *curr_node = root;
     while(curr_node != NULL)
     {
-        cout << curr_node -> data << " ";
-        curr_node = curr_node ->next;
+        cout << curr_node->data << " ";
+        curr_node = curr_node->next;
     }
     cout << endl;
 }
 
-void insertAtBeginning(int value)
+void insertAtEnd(int value)
 {
     node *newNode = new node();  // Create a new node
     newNode->data = value;       // Set its data
+    newNode->next = NULL;        // It will be the last node, so its next is NULL
 
-    newNode->next = root;        // Make it point to the current root //link korechi
-
-    root = newNode;              // Update root to the new node
+    if (root == NULL) {
+        root = newNode;          // If the list is empty, make the new node the root
+    } else {
+        node *temp = root;
+        while (temp->next != NULL) {
+            temp = temp->next;     // Traverse to the last node
+        }
+        temp->next = newNode;      // Make the last node point to the new node
+    }
 }
 
 int main()
 {
     int num;
-    cout << "How many values do you insert in the list : ";
+    cout << "How many values do you insert in the list: ";
     cin >> num;
 
     node *p_i = new node[num];  // Allocate memory for all nodes
@@ -38,7 +48,7 @@ int main()
     // Initialize root to point to the first node
     root = &p_i[0];
 
-    cout << "Enter " << num << " values :";
+    cout << "Enter " << num << " values: ";
 
     node *temp = root; // To traverse the list
     for(int i = 0; i < num; i++)
@@ -56,16 +66,16 @@ int main()
     }
     printing();
 
-    cout << "Enter value to insert at the beginning: ";
-    int valueToInsert;
-    cin >> valueToInsert;
 
-    insertAtBeginning(valueToInsert);
+    cout << "Enter value to insert at the end: ";
+    int valueToInsertEnd;
+    cin >> valueToInsertEnd;
 
-    cout << "Updated list after inserting at the beginning:\n";
+    insertAtEnd(valueToInsertEnd);
+
+    cout << "Updated list after inserting at the end:\n";
     printing();
-
-
 
     return 0;
 }
+
