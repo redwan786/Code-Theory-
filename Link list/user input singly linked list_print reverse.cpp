b1515a9e2f44703374c4578ea106cb1e9fe86f7d
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-01-31 20:50:59 +0600

    user input singly linked list_print serially

---
+++ Link list/user input singly linked list_print reverse.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+node *root = NULL;
+
+void printing(node *curr_node)
+{
+    if (curr_node == NULL){
+        return;
+    }
+    cout << curr_node->data << " ";
+    printing(curr_node->next);
+
+}
+
+int main()
+{
+    int num;
+    cout << "How many values do you insert in the list : ";
+    cin >> num;
+
+    node *p = new node[num]; // Allocate memory for all nodes
+
+    // Initialize root to point to the first node
+    root = &p[0];
+
+    cout << "Enter " << num << " values :";
+
+    node *temp = root; // To traverse the list
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> temp->data;
+
+        if (i == (num - 1))
+        {
+            temp->next = NULL;
+        }
+        else
+        {
+            temp->next = &p[i + 1]; // Link to the next allocated node
+            temp = temp->next;
+        }
+    }
+
+    printing(root);
+
+    // Don't forget to release the allocated memory
+    delete[] p;
+
+    return 0;
+}
+
