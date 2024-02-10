50d6ad327d8e00a926906e5846d0f1cb6aa5191e
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-02-10 22:54:07 +0600

    update

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h> // Including the entire standard library
 using namespace std;
 
-int main() {
+int main()
+{
     queue<int> q; // Creating an empty queue of integers
     q.push(10);   // Pushing 10 onto the queue
     q.push(20);   // Pushing 20 onto the queue
@@ -13,10 +14,14 @@ int main() {
     if (q.empty()) // Check if the queue is empty
     {
         cout << "Queue is empty"; // Display message if queue is empty
-    } else {
+    }
+    else
+    {
 
-        cout << "First element: " << q.front() << endl;
-        cout << "Last element: " << q.back()<< endl;
+        // Displaying the first and last elements of the queue
+        cout << "First element: " << q.front() << endl; // Display the first element of the queue
+        cout << "Last element: " << q.back()<< endl; // Display the last element of the queue
+
         while (!q.empty()) // Loop until queue is empty
         {
             int val = q.front(); // Get the front element of the queue
