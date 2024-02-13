7ee2625d52c5cbb78a45d120d71f171d04b17677
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2024-02-13 23:32:56 +0600

    fixed

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,27 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node
+{
     vector<int> neighbors;
     bool visited = false;
 };
 
 vector<Node> Node_Vec;
 
-void BFS(int starting_node) {
+void BFS(int starting_node)
+{
     queue<int> Q;
     Node_Vec[starting_node].visited = true;
     Q.push(starting_node);
 
-    while (!Q.empty()) {
+    while (!Q.empty())
+    {
         int u = Q.front();
         Q.pop();
         cout << u << " ";
 
-        for (int v : Node_Vec[u].neighbors) {
-            if (!Node_Vec[v].visited) {
+        for (int v : Node_Vec[u].neighbors)
+        {
+            if (!Node_Vec[v].visited)
+            {
                 Node_Vec[v].visited = true;
                 Q.push(v);
             }
@@ -29,19 +32,28 @@ void BFS(int starting_node) {
     }
 }
 
-int main() {
+int main()
+{
     int node_no, edge_no;
     cout << "Enter the number of nodes: ";
     cin >> node_no;
     cout << "Enter the number of edges: ";
     cin >> edge_no;
 
-    Node_Vec.resize(node_no);
+    Node_Vec.resize(node_no + 1);
 
-    for (int i = 0; i < edge_no; i++) {
+    for (int i = 1; i <= edge_no; i++)
+    {
         int nodeA, nodeB;
-        cout << "Enter nodes for edge " << i + 1 << ": ";
+        cout << "Enter nodes for edge " << i << ": ";
         cin >> nodeA >> nodeB;
+
+        // Bounds checking
+        if (nodeA < 1 || nodeA > node_no || nodeB < 1 || nodeB > node_no)
+        {
+            cout << "Invalid node number. Exiting.\n";
+            return 1;
+        }
 
         Node_Vec[nodeA].neighbors.push_back(nodeB);
         Node_Vec[nodeB].neighbors.push_back(nodeA);
@@ -50,6 +62,13 @@ int main() {
     int starting_node;
     cout << "Enter the starting node for BFS: ";
     cin >> starting_node;
+
+    // Bounds checking
+    if (starting_node < 1 || starting_node > node_no)
+    {
+        cout << "Invalid starting node. Exiting.\n";
+        return 1;
+    }
 
     cout << "BFS traversal starting from node " << starting_node << ": ";
     BFS(starting_node);
